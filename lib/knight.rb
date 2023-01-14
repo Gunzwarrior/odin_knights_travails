655ebb75cff56412bd644d14ea1d2aff2a27eb83
Thomas Rialland <rialland.thomas@hotmail.fr>
2023-01-14 12:50:49 +0100

    Lay out some plans

---
+++ lib/knight.rb
@@ -0,0 +1,16 @@
+=begin
+knight class
+- occupy a set of possible coordinates (0..width-1, 0..height-1)  
+- next possible moves = x+1,y+2 / x+1,y-2 / x-1,y+2 / x-1,y-2
+                      x+2,y+1 / x+2,y-1 / x-2,y+1 / x-2,y-1
+- all those moves are attributes set to the coordinates or nil
+  if out of bounds or already traveled
+
+- not possible if outside possible coordinates or already went there
+- max total move/branch width*height-1 (starting position)
+- keep array of already traveled 
+- stop at set coordinates
+- find shortest route = shortest array
+- display said array to show shortest route
+
+=end
