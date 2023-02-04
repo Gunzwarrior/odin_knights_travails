79ec572329359733f6f48f918590a09cebfcc09a
Thomas Rialland <rialland.thomas@hotmail.fr>
2023-02-04 10:07:08 +0100

    Clean knight.rb code

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,35 +1,19 @@
-=begin
-knight class
-- occupy a set of possible coordinates (0..width-1, 0..height-1)  
-- next possible moves = x+1,y+2 / x+1,y-2 / x-1,y+2 / x-1,y-2
-                      x+2,y+1 / x+2,y-1 / x-2,y+1 / x-2,y-1
-- all those moves are attributes set to the coordinates or nil
-  if out of bounds or already traveled
+# frozen_string_literal: true
 
-- not possible if outside possible coordinates or already went there
-- max total move/branch width*height-1 (starting position)
-- keep array of already traveled 
-- stop at set coordinates
-- find shortest route = shortest array
-- display said array to show shortest route
-- change board.build_path to allow coming back to visited spots
-
-=end
+# Class reprensenting the possible knight moves
 class Knight
   attr_accessor :coordinates, :left_up, :left_down, :right_up,
                 :right_down, :up_left, :up_right, :down_left, :down_right
 
-def initialize(coordinates, left_up = nil, left_down = nil,
-               right_up = nil, right_down = nil, up_left = nil,
-               up_right = nil, down_left = nil, down_right = nil)
-  @coordinates = coordinates
-  @left_up = left_up
-  @left_down = left_down
-  @right_up = right_up
-  @right_down = right_down
-  @up_left = up_left
-  @up_right = up_right
-  @down_left = down_left
-  @down_right = down_right
-end
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @left_up = nil
+    @left_down = nil
+    @right_up = nil
+    @right_down = nil
+    @up_left = nil
+    @up_right = nil
+    @down_left = nil
+    @down_right = nil
+  end
 end
