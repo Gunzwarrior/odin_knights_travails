685e654786beb693e694935727e9d6d050aa8bbc
Thomas Rialland <rialland.thomas@hotmail.fr>
2023-02-04 10:01:45 +0100

    Clean main.rb code

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require_relative 'board'
 
 board = Board.new
 
 board.build_board
 board.build_links
-board.root = board.board[[0,0]]
+board.root = board.board[[0, 0]]
 
-board.knight_moves([1,2],[2,3])
+board.knight_moves([1, 2], [2, 3])
